78fd1bdb207354a4d0b568fdffa86106224cd724
Tech-Devam <devamtech31@gmail.com>
2026-05-11 14:10:12 +0530

    first commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 4000
 
 // ─── Middleware ───────────────────────────────────────────────────────────────
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+  origin: process.env.CORS_ORIGIN || 'https://portfolio-nine-pi-v723j4hkej.vercel.app',
   methods: ['GET', 'POST'],
 }))
 
